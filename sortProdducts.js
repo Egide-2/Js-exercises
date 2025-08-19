c8f43fb4d890ab5d44bae316a234e0d1388f7c8c
Egide-2 <EgideBilly69@gmail.com>
2025-08-19 15:02:34 +0200

    this is how can work on the values in object

--- sortProdducts.js
+++ sortProdducts.js
@@ -1,8 +1,15 @@
-const products = [
-    {name:"shoes",price:7000},
-    {name:"phone",price:700},
-     {name:"pc",price:900}
-]
+const campany = {
+   name: "PGEE",
+   servise: undefined,
+   address:null,
+   direction:"kigali"
 
-const sorted = products.sort((a,b)=> a.price - b.price)
-console.log(sorted)
+
+}
+
+for (let key in campany){
+    if(campany[key] === null ){  campany[key] =   0 }
+    else if(campany[key] === undefined){ campany[key] = ""}
+}
+
+console.log(campany)
